c5d4e07b463c8d228aba34be254678924005784d
Andres <andresinsuastyd10@gmail.com>
2020-12-25 18:07:36 -0500

    reto ultima letra del nombre

--- clase1.js
+++ clase1.js
@@ -13,3 +13,6 @@ var nombreCompleto3 = `${nombre} ${apellido.toUpperCase()}`
 
 var str = nombre.charAt(1)+nombre.charAt(2)
 var str2 = nombre.substr(1,2)
+
+var ultimaLetra = nombreCompleto.charAt(nombreCompleto.length-1)
+var ultimaLetra2 = nombreCompleto.slice(-1)
