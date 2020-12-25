3cd5af023b6f71751829bd070a51d9f1abe3a29d
Andres <andresinsuastyd10@gmail.com>
2020-12-25 18:23:50 -0500

    manejo de numeros

---
+++ 2clase - numeros.js
@@ -0,0 +1,26 @@
+var edad = 25
+edad += 1 
+
+var peso = 75
+// peso = peso -2
+peso -= 2
+
+var sandwich = 1
+
+peso = peso + sandwich
+
+var deporte = 3
+
+peso = peso - deporte
+
+
+var precioDeVino = 200.3
+
+var total = precioDeVino * 3
+var total2 = Math.round(precioDeVino *100*3)/100
+var totalStr = total.toFixed(2)
+var totalNum = parseFloat(totalStr)
+
+var pizza = 8
+var persona = 2
+var cantidadPersona = pizza/persona
