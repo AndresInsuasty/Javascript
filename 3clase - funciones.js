dde249d7cc11003d0c57bc651fab3ee300379231
Andres <andresinsuastyd10@gmail.com>
2020-12-25 18:33:32 -0500

    explorando funciones

---
+++ 3clase - funciones.js
@@ -0,0 +1,11 @@
+var nombre = 'Andres'
+var edad = 25
+
+function imprimirEdad(n,e){
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(n=nombre,e=edad)
+imprimirEdad('Diana',28)
+imprimirEdad('Erick',24)
+imprimirEdad('Dario',27)
