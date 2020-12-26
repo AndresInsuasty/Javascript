c346addb9fd2e51686f6053396c14cebd2021789
Andres <andresinsuastyd10@gmail.com>
2020-12-25 21:14:21 -0500

    reto de objetos, nombre y edad

---
+++ 6clase - Desestructurar objetos.js
@@ -0,0 +1,31 @@
+var andres = {
+    nombre: 'Andres',
+    apellido: 'Insuasty',
+    edad: 25
+}
+
+var diana = {
+    nombre: 'Diana',
+    apellido: 'Narvaez',
+    edad: 28
+}
+
+function imprimirMayusculas(persona){
+    var {nombre}=persona
+    var nombre = nombre.toUpperCase()
+    console.log(nombre)
+}
+
+imprimirMayusculas(andres)
+imprimirMayusculas(diana)
+imprimirMayusculas({nombre: 'Pepito',edad:10})
+
+function imprimirNombreyEdad(persona){
+    var {nombre} = persona
+    var {edad} = persona
+    console.log(`Hola, me llamo ${nombre} y tengo ${edad} años`)
+}
+
+imprimirNombreyEdad(andres)
+imprimirNombreyEdad(diana)
+imprimirNombreyEdad({nombre: 'Pepito',edad:10})
