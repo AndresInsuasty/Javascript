a011c8cf6d7244a65bb42558a106c5b6e64ebbfd
Andres <andresinsuastyd10@gmail.com>
2020-12-26 20:39:18 -0500

    filtrado de arrays

---
+++ 17clase - filtrar arrays.js
@@ -0,0 +1,38 @@
+var andres = {
+    nombre: 'Andres',
+    apellido: 'Insuasty',
+    altura: 1.73
+}
+var diana = {
+    nombre: 'Diana',
+    apellido: 'Narvaes',
+    altura: 1.68
+}
+var santiago = {
+    nombre: 'Santiago',
+    apellido: 'Insuasty',
+    altura: 1.2
+}
+
+var papa = {
+    nombre: 'German',
+    apellido: 'Insuasty',
+    altura: 1.68
+}
+
+var mama = {
+    nombre: 'Amada',
+    apellido: 'Delgado',
+    altura: 1.65
+}
+
+
+const esAlta = (persona) => {
+    return persona.altura > 1.65
+}
+
+var personas = [andres, diana, santiago, papa, mama]
+
+var personasAltas = personas.filter(esAlta)
+
+console.log(personasAltas)
