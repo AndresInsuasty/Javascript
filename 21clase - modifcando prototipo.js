a7135a90f2acc428d215670315c0ceb0892e732e
Andres <andresinsuastyd10@gmail.com>
2020-12-29 18:06:57 -0500

    modificando prototipos

---
+++ 21clase - modifcando prototipo.js
@@ -0,0 +1,24 @@
+function persona(nombre,apellido,altura=1.7){
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+   
+   }
+   
+   persona.prototype.saludar = function(){
+       console.log(`Hola me llamo ${this.nombre} ${this.apellido}`)
+       if (this.altura>1.75){
+           console.log('Soy una persona alta')
+       }else{
+           console.log('No soy una persona alta')
+           console.log('********')
+       }
+   }
+   
+   var andres = new persona('Andres','Insuasty',1.73)
+   var diana = new persona(nombre='Diana',apellido='Narvaez',1.68)
+   var pepito = new persona('Pepito','Perez',1.80)
+   
+   andres.saludar()
+   diana.saludar()
+   pepito.saludar()
